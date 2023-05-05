bc3e00b1a4838836f3930e3d24d4d5c2e9d370fd
Education-cite <tusarjobs@gmail.com>
2023-05-05 22:25:12 +0600

    r

--- src/raf_Practice/MainClass.java
+++ src/raf_Practice/MainClass.java
@@ -4,23 +4,13 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MainClass {
+	
 	public static void main(String[] args) {
 		
-		int num1=60;
-		int num2=24;
-		int r ;
-
 		
-		while(num2!=0) {
-			
-			 r =num1%num2;
-			num1=num2;
-			num2=r;
-			
-		}
-		
-		System.out.println(num1);
+		    
+		   }  
 		
 	}
 
-}
+
